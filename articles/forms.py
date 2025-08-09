0aff1c5a4c31c1c8a71b765174533a8f89e46b98
samuel <legosthes@gmail.com>
2025-08-09 20:35:34 +0800

    feat: create new articles

---
+++ articles/forms.py
@@ -0,0 +1,14 @@
+from .models import Article
+from django.forms import ModelForm, TextInput, Textarea, CheckboxInput
+
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ["title", "content", "is_published"]
+        labels = {"title": "Title", "content": "Content", "is_published": "Publish"}
+        widgets = {
+            "title": TextInput(attrs={"class": "input w-full"}),
+            "content": Textarea(attrs={"class": "textarea w-full", "rows": 5}),
+            "is_published": CheckboxInput(attrs={"class": "checkbox"}),
+        }
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .forms import ArticleForm
+from django.views.decorators.http import require_POST
+
 
 # Create your views here.
+@require_POST
 def index(request):
-    return render(request,"articles/index.html")
+    form = ArticleForm(request.POST)
+    form.save()
+
+    return render(request, "articles/index.html")
+
+
+def new(request):
+    form = ArticleForm(request.POST)
+    return render(request, "articles/new.html", {"form": form})
